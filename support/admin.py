a4d8cb6928ea33fe0cb38e45cc89192a6e60239b
Seungho Hwang <tmdgh717@naver.com>
2022-04-28 18:43:21 +0900

    미션 코드 추가

--- support/admin.py
+++ support/admin.py
@@ -1,6 +1,13 @@
 from django.contrib import admin
-from .models import FAQ
+from .models import FAQ,Inquiry,Answer
 # Register your models here.
+class AnswerInline(admin.TabularInline):
+    model = Answer
+    # extra = 0
+    min_num = 1
+    max_num = 1
+    verbose_name = '답변'
+    # verbose_name_plural = '답변' #답변을 1개만 달려고 복수형은 없앴고, 최소,최대 갯수를 1개로 고정시켰다.
 
 @admin.register(FAQ)
 class FAQModelAdmin(admin.ModelAdmin) :
@@ -11,4 +18,11 @@ class FAQModelAdmin(admin.ModelAdmin) :
     list_filter = ('category',) #필터필드 만드는 거 이것도 마찬가지로 ,있어야함
     
 
-    # inlines = [CommentInline]
+@admin.register(Inquiry)
+class InquiryModelAdmin(admin.ModelAdmin) :
+    list_display = ('title','category','created_at','writer') 
+    search_fields = ('title','email','phone') 
+    search_help_text = '제목,이메일,전화번호 검색이 가능합니다!' 
+    list_filter = ('category',)
+
+    inlines = [AnswerInline]
